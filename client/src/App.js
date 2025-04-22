0075732d88d5809b56e7cbc39a76cc5a05b8563b
TolkovaT <ibragimov.artem12@gmail.com>
2025-04-23 01:14:08 +0400

    carCard в app

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
 import MainPage from './pages/MainPage';
 import AuthForm from './pages/AuthForm';
+import CarCard from './components/CarCard';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/auth" element={<AuthForm />} />
         <Route path="/reg" element={<Register />} />
         <Route path="/" element={<MainPage />} />
+        <Route path="/carCard" element={<CarCard />} />
 
       </Routes>
     </Router>
