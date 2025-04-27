4134d8da2b5bac8f5868fd3233cf9a05ea50a781
alina <alina.chubar@mail.ru>
2025-04-28 01:04:32 +0400

    Добавила синхронизацию активной ссылки с текущим URL

--- client/src/components/HeaderC.js
+++ client/src/components/HeaderC.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react'; 
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import { useLocation } from 'react-router-dom'; 
 import { FaUser, FaHeart, FaCar } from 'react-icons/fa';
 import '../styles/HeaderC.css';
 
@@ -8,9 +9,20 @@ import '../styles/HeaderC.css';
 const HeaderC = () => {
 
   const [activeLink, setActiveLink] = useState('main'); 
-  const [isAdmin, setIsAdmin] = useState(true); 
-  const [isAuthenticated, setIsAuthenticated] = useState(true);
-  
+  const [isAdmin, setIsAdmin] = useState(true); //времено 
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const location = useLocation();
+
+     // Синхронизация активной ссылки с текущим URL
+     useEffect(() => {
+        const path = location.pathname;
+        if (path === '/') setActiveLink('main');
+        else if (path === '/favorites') setActiveLink('favorites');
+        else if (path === '/add-car') setActiveLink('add-car');
+        else if (path === '/profile') setActiveLink('profile');
+        else if (path === '/auth' || path === '/reg') setActiveLink('auth');
+      }, [location]);
+      
   //Дорелизовать проверку прав пользователя
 
   return (
@@ -27,14 +39,12 @@ const HeaderC = () => {
                     <a 
                         href="/" 
                         className={`nav-link ${activeLink === 'main' ? 'active' : ''}`}
-                        onClick={() => setActiveLink('main')}
                     >
                         Главный листинг
                     </a>
                     <a 
                         href="/favorites" 
                         className={`nav-link ${activeLink === 'favorites' ? 'active' : ''}`}
-                        onClick={() => setActiveLink('favorites')}
                     >
                          Избранное <FaHeart className="heart-icon" />
                     </a>
@@ -42,25 +52,24 @@ const HeaderC = () => {
                         <a 
                             href="/add-car" 
                             className={`nav-link ${activeLink === 'add-car' ? 'active' : ''}`}
-                            onClick={() => setActiveLink('add-car')}
                         >
                             Добавить машину
                         </a>
                     )}
-                   {isAuthenticated && <a 
-                        href="/profile" 
-                        className={`nav-link ${activeLink === 'profile' ? 'active' : ''}`}
-                        onClick={() => setActiveLink('profile')}
-                    >
-                        Профиль <FaUser className="user-icon" />
-                    </a>}
-                    <a 
-                        href="/auth" 
-                        className={`nav-link ${activeLink === 'auth' ? 'active' : ''}`}
-                        onClick={() => setActiveLink('auth')}
-                    >
-                        Зарегистрироваться/Войти <FaUser className="user-icon" />
-                    </a>
+                   {isAuthenticated ? (<a 
+                            href="/profile" 
+                            className={`nav-link ${activeLink === 'profile' ? 'active' : ''}`}
+                        >
+                            Профиль <FaUser className="user-icon" />
+                        </a>
+                    ) : (
+                       <a 
+                           href="/auth" 
+                           className={`nav-link ${activeLink === 'auth' ? 'active' : ''}`}
+                       >
+                           Зарегистрироваться/Войти <FaUser className="user-icon" />
+                       </a>
+                    )}
                 </div>
             </div>
         </nav>
